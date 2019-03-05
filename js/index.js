1bbe33a2647377183875d1319c0652afaf4e7574
Adetunji <ashennaike@gmail.com>
2019-03-05 16:16:35 -0500

    fixed the issue had to change the listerner to listen to container

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Your code goes here
 
+//required item for several event listeners to work
+const container = document.querySelector('body');
+
 //hover color change and unchange
 let navItems = document.querySelector('.nav');
 navItems.addEventListener('mouseover',function(event){
@@ -14,12 +17,12 @@ navItems.addEventListener('mouseout',function(event){
 let img1 = document.querySelector('.content-section img');
 let img2 = document.querySelector('.inverse-content img');
 
-img1.addEventListener('keydown',function(){
+container.addEventListener('keydown',function(){
   img1.src = 'img/fun.jpg';
   img2.src = 'img/adventure.jpg'; 
 });
 
-img1.addEventListener('keyup',function(){
+container.addEventListener('keyup',function(){
   img2.src = 'img/fun.jpg';
   img1.src = 'img/adventure.jpg';
 });
@@ -27,7 +30,7 @@ img1.addEventListener('keyup',function(){
 
 //wheel h1 text color change 
 const logo = document.querySelector('.logo-heading');
-const container = document.querySelector('body');
+
 container.addEventListener('wheel',function(){
   let hexColorCode = '#' + (function co(lor){   return (lor +=
     [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
